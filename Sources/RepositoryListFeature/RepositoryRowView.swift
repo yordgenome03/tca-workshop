47e8679a0ca41eafe1c4ea4d30581263e8631c0e
YomEngine <120723438+yordgenome03@users.noreply.github.com>
2024-09-12 10:41:27 +0900

    RowView の実装

--- Sources/RepositoryListFeature/RepositoryRowView.swift
+++ Sources/RepositoryListFeature/RepositoryRowView.swift
@@ -31,3 +31,66 @@ public struct RepositoryRow {
         }
     }
 }
+
+// MARK: - RepositoryRowView
+
+import SwiftUI
+
+struct RepositoryRowView: View {
+    let store: StoreOf<RepositoryRow>
+    
+    init(store: StoreOf<RepositoryRow>) {
+        self.store = store
+    }
+    
+    var body: some View {
+        Button {
+            store.send(.rowTapped)
+        } label: {
+            VStack(alignment: .leading, spacing: 8) {
+                Text(store.repository.fullName)
+                    .font(.title2.bold())
+                Text(store.repository.description ?? "")
+                    .font(.body)
+                    .lineLimit(2)
+                HStack(alignment: .center, spacing: 32) {
+                    Label(
+                        title: {
+                            Text("\(store.repository.stargazersCount)")
+                                .font(.callout)
+                        },
+                        icon: {
+                            Image(systemName: "star.fill")
+                                .foregroundStyle(.yellow)
+                        }
+                    )
+                    Label(
+                        title: {
+                            Text(store.repository.language ?? "")
+                                .font(.callout)
+                        },
+                        icon: {
+                            Image(systemName: "text.word.spacing")
+                                .foregroundStyle(.gray)
+                        }
+                    )
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .buttonStyle(.plain)
+    }
+}
+
+#Preview {
+    RepositoryRowView(
+        store: .init(
+            initialState: RepositoryRow.State(
+                repository: .mock(id: 1)
+            )
+        ) {
+            RepositoryRow()
+        }
+    )
+    .padding()
+}
