62c4c28f3d38e409495f842a71f2c0ee360149ef
YomEngine <120723438+yordgenome03@users.noreply.github.com>
2024-09-16 06:00:06 +0900

    swift-dependencies による依存関係の制御

--- Sources/GitHubAPIClient/Client.swift
+++ Sources/GitHubAPIClient/Client.swift
@@ -1,1 +1,51 @@
+import Dependencies
+import Entity
+import Foundation
+import XCTestDynamicOverlay
 
+public struct GithubAPIClient {
+    public var searchRepositories: @Sendable (_ query: String) async throws -> [Repository]
+}
+
+extension GithubAPIClient: DependencyKey {
+    public static let liveValue = Self(
+        searchRepositories: { query in
+            let url = URL(string: "https://api.github.com/search/repositories?q=\(query)&sort=stars")!
+            var request = URLRequest(url: url)
+            if let token = Bundle.main.infoDictionary?["GitHubPersonalAccessToken"] as? String {
+                request.setValue(
+                    "Bearer \(token)",
+                    forHTTPHeaderField: "Authorization")
+            }
+            let (data, _) = try await URLSession.shared.data(for: request)
+            let repositories = try jsonDecoder.decode(
+                GithubSearchResult.self,
+                from: data
+            ).items
+            return repositories
+        }
+    )
+}
+
+private let jsonDecoder: JSONDecoder = {
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    return decoder
+}()
+
+extension GithubAPIClient: TestDependencyKey {
+    public static let previewValue = Self(
+        searchRepositories: { _ in [] }
+    )
+    
+    public static let testValue = Self(
+        searchRepositories: unimplemented("\(Self.self).searchRepositories")
+    )
+}
+
+extension DependencyValues {
+    public var gitHubAPIClient: GithubAPIClient {
+        get { self[GithubAPIClient] }
+        set { self[GithubAPIClient] = newValue }
+    }
+}
