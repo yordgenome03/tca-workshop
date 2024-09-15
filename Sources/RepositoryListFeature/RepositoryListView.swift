fbf282513f1cce6826786d2207ae6c37fa65f622
YomEngine <120723438+yordgenome03@users.noreply.github.com>
2024-09-16 05:43:14 +0900

    Reducer への検索機能の追加

--- Sources/RepositoryListFeature/RepositoryListView.swift
+++ Sources/RepositoryListFeature/RepositoryListView.swift
@@ -23,10 +23,15 @@ public struct RepositoryList {
         case onAppear
         case searchRepositoriesResponse(Result<[Repository], Error>)
         case repositoryRows(IdentifiedActionOf<RepositoryRow>)
+        case queryChangeDebounced
         case binding(BindingAction<State>)
     }
     
     public init() {}
+    
+    private enum CancelID {
+        case response
+    }
     
     public var body: some ReducerOf<Self> {
         BindingReducer()
@@ -34,28 +39,7 @@ public struct RepositoryList {
             switch action {
             case .onAppear:
                 state.isLoading = true
-                return .run { send in
-                    await send(
-                        .searchRepositoriesResponse(
-                            Result {
-                                let query = "composable"
-                                let url = URL(string: "https://api.github.com/search/repositories?q=\(query)&sort=stars")!
-                                var request = URLRequest(url: url)
-                                if let token = Bundle.main.infoDictionary?["GitHubPersonalAccessToken"] as? String {
-                                    request.setValue(
-                                        "Bearer \(token)",
-                                        forHTTPHeaderField: "Authorization")
-                                }
-                                let (data, _) = try await URLSession.shared.data(for: request)
-                                let repositories = try jsonDecoder.decode(
-                                    GithubSearchResult.self,
-                                    from: data
-                                ).items
-                                return repositories
-                            }
-                        )
-                    )    
-                }
+                return searchRepositories(by: "composable")
             case let .searchRepositoriesResponse(result):
                 state.isLoading = false
                 
@@ -73,12 +57,53 @@ public struct RepositoryList {
                 }
             case .repositoryRows:
                 return .none
+            case .binding(\.query):
+                return .run { send in
+                    await send(.queryChangeDebounced)
+                }
+                .debounce(
+                    id: CancelID.response,
+                    for: .seconds(0.3),
+                    scheduler: DispatchQueue.main.eraseToAnyScheduler()
+                )
+            case .queryChangeDebounced:
+                guard !state.query.isEmpty else {
+                    return .none
+                }
+                
+                state.isLoading = true
+                
+                return searchRepositories(by: state.query)
             case .binding:
                 return .none
             }
         }
         .forEach(\.repositoryRows, action: \.repositoryRows) {
             RepositoryRow()
+        }
+    }
+    
+    func searchRepositories(by query: String) -> Effect<Action> {
+        .run { send in
+            await send(
+                .searchRepositoriesResponse(
+                    Result {
+                        let url = URL(string: "https://api.github.com/search/repositories?q=\(query)&sort=stars")!
+                        var request = URLRequest(url: url)
+                        if let token = Bundle.main.infoDictionary?["GitHubPersonalAccessToken"] as? String {
+                            request.setValue(
+                                "Bearer \(token)",
+                                forHTTPHeaderField: "Authorization")
+                        }
+                        let (data, _) = try await URLSession.shared.data(for: request)
+                        let repositories = try jsonDecoder.decode(
+                            GithubSearchResult.self,
+                            from: data
+                        ).items
+                        return repositories
+                    }
+                )
+            )    
         }
     }
     
